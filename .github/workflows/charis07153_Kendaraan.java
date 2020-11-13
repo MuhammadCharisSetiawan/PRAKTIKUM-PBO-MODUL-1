908352e20b8adf7ff67724811bfcd3df1d0855ce
muhammadcharis07153 <74410724+muhammadcharis07153@users.noreply.github.com>
2020-11-13 19:39:51 +0700

    Create charis07153_Kendaraan.java

---
+++ .github/workflows/charis07153_Kendaraan.java
@@ -0,0 +1,20 @@
+package charis07103_main;
+
+public class charis07153_Kendaraan {
+    String charis07153_platnomor;
+    String charis07153_jeniskendaraan;
+ 
+    public charis07153_Kendaraan(String charis07153_platnomor,String charis07103_jeniskendaraan){
+        this.charis07153_platnomor = charis07153_platnomor;
+        this.charis07153_jeniskendaraan = charis07103_jeniskendaraan;
+    }
+    
+    String getcharis07153_Platnomor() {
+        return this.charis07153_platnomor;
+    }
+    
+    String getcharis07153_Jeniskendaraan() {
+        return this.charis07153_jeniskendaraan;
+    }   
+ 
+}
